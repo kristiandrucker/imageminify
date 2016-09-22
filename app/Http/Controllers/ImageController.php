4cf40663efa1041ff07196a3b1a9695f0ddcb165
Kristian <oxkipo@googlemail.com>
2016-09-22 10:49:52 +0200

    Add s3 support

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
@@ -13,6 +14,13 @@ class ImageController extends Controller
 	
 	public function index($options, $slug)
 	{
+	    $name = implode('-', explode(',', $options)) . '&' . implode('-', explode('/', $slug));
+        if($this->getExtension($name) == 'gif') {
+            return response(file_get_contents(env('BASE_IMG') . $slug), 200)->header('Content-Type', 'image/gif');
+        }
+        if(Storage::exists(env('S3_URL') . '/' . $name)) {
+            return response(Storage::get(env('S3_URL') . '/' . $name), 200)->header('Content-Type', 'image/jpeg');
+        }
 		if($options == 'd')
 		{
 			$options = config('image.default');
@@ -28,35 +36,34 @@ class ImageController extends Controller
 		}
 		$options = $this->explodeOptions($options);
 		$image = $this->generateImage($options, $slug);
-		return $image;
+        Storage::put(env('S3_URL') . '/' . $name, $image);
+		return response($image, 200)->header('Content-Type', 'image/jpeg');
     }
 	
 	public function generateImage($options, $slug)
 	{
-		$image = Image::cache(function($img) use($options, $slug) {
-			$img = $img->make(env('BASE_IMG') . $slug);
+		$img = Image::make(env('BASE_IMG') . $slug);
 			
-			foreach($options as $key => $option) {
-				$val = explode(':', preg_replace('/[^0-9]/', ':', $option));
-				$key = preg_replace('/[^a-zA-Z]/', '', $option);
-				$func = $this->functions($key);
-				if($val[0] == null) {
-					$val[0] = null;
-				} elseif($val[1] == null) {
-					$val[1] = null;
-				}
-				if($key == 'x') {
-					$img = $img->$func($val[0], $val[1], function($constraint) {
-						$constraint->aspectRatio();
-					});
-				} elseif($key == 'q') {
-					$this->quality = $val[1];
-				} else {
-					$img = $img->$func($val[1]);
-				}
-			}
-		}, 86400, true);
-		return $image->response($this->getExtension($slug), $this->quality);
+        foreach($options as $key => $option) {
+            $val = explode(':', preg_replace('/[^0-9]/', ':', $option));
+            $key = preg_replace('/[^a-zA-Z]/', '', $option);
+            $func = $this->functions($key);
+            if($val[0] == null) {
+                $val[0] = null;
+            } elseif($val[1] == null) {
+                $val[1] = null;
+            }
+            if($key == 'x') {
+                $img = $img->$func($val[0], $val[1], function($constraint) {
+                    $constraint->aspectRatio();
+                });
+            } elseif($key == 'q') {
+                $this->quality = $val[1];
+            } else {
+                $img = $img->$func($val[1]);
+            }
+        }
+		return (string) $img->encode($this->getExtension($slug), $this->quality);
     }
     
 	public function explodeOptions($options)
